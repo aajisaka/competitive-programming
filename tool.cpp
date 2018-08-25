8491a7dd019298cd93effae6bfa076e67d82b4be
Akira Ajisaka <aajisaka@apache.org>
2018-08-25 20:54:34 +0900

    Add mod_pow function in tool.cpp.

--- tool.cpp
+++ tool.cpp
@@ -16,3 +16,16 @@ void swap(int &a, int &b) {
   b = a;
   a = tmp;
 }
+
+// return a^x (mod mod)
+long long mod_pow(long long a, long long x, long long mod) {
+  long long res = 1;
+  while(x > 0) {
+    if (x & 1) {
+      res = res * a % mod;
+    }
+    res = res * res % mod;
+    x >>= 1;
+  }
+  return res;
+}
