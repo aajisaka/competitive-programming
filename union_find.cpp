5b5bc6ef6d44e66a2f33882923c8e9e9d3df9b5c
Akira Ajisaka <aajisaka@apache.org>
2018-12-18 04:33:34 +0900

    Rename rank to rak to avoid conflict with std::rank.

--- union_find.cpp
+++ union_find.cpp
@@ -1,12 +1,12 @@
 #include<vector>
 
 std::vector<int> par(200005);
-std::vector<int> rank(200005);
+std::vector<int> rak(200005);
 
 void init(int n) {
   for(int i=1; i<=n; i++) {
     par.at(i) = i;
-    rank.at(i) = 0;
+    rak.at(i) = 0;
   }
 }
 
@@ -22,11 +22,11 @@ void unite(int x, int y) {
   x = find(x);
   y = find(y);
   if (x==y) return;
-  if (rank.at(x) < rank.at(y)) {
+  if (rak.at(x) < rak.at(y)) {
     par.at(x) = y;
   } else {
     par.at(y) = x;
-    if (rank.at(x) == rank.at(y)) rank.at(x)++;
+    if (rak.at(x) == rak.at(y)) rak.at(x)++;
   }
 }
 
