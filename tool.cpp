1fe535574f625e8fc0e473a7298958898196a3c8
Akira Ajisaka <aajisaka@apache.org>
2019-01-27 03:04:58 +0900

    Make the code shorter

--- tool.cpp
+++ tool.cpp
@@ -4,9 +4,7 @@
 long long gcd(long long a, long long b) {
   long long r = 1;
   while (r != 0) {
-    r = a%b;
-    a = b;
-    b = r;
+    r = a%b; a = b; b = r;
   }
   return a;
 }
@@ -15,11 +13,8 @@ long long gcd(long long a, long long b) {
 long long mod_pow(long long a, long long x, long long mod) {
   long long res = 1;
   while(x > 0) {
-    if (x & 1) {
-      res = res * a % mod;
-    }
-    res = res * res % mod;
-    x >>= 1;
+    if (x & 1) res = res * a % mod;
+    res = res * res % mod; x >>= 1;
   }
   return res;
 }
