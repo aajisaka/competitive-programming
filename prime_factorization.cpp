d340074cbb8b003c03a87aba6678515d2f0b2e91
Akira Ajisaka <aajisaka@apache.org>
2019-02-11 07:11:35 +0900

    Add prime_factorization function that returns map

--- prime_factorization.cpp
+++ prime_factorization.cpp
@@ -1,5 +1,6 @@
 #include<map>
 using namespace std;
+using ll = long long;
 
 void prime_factorization(long long a, map<long long, int>& mp) {
   for(long long i=2; i*i<=a; i++) {
@@ -11,4 +12,13 @@ void prime_factorization(long long a, map<long long, int>& mp) {
   if (a > 1) {
     mp[a]++;
   }
+}
+
+map<ll, int> prime_factorization(ll a) {
+  map<ll, int> mp;
+  for(ll i=2; i*i<=a; i++) {
+    while (a%i == 0) { mp[i]++; a /= i; }
+  }
+  if (a > 1) mp[a]++;
+  return mp;
 }
