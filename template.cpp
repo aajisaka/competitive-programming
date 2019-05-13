21553a228b001b39577e37e95fec440ef8a2d960
Akira Ajisaka <aajisaka@apache.org>
2019-05-14 04:34:51 +0900

    Moved from VSCode to CLion, some setting changed

--- template.cpp
+++ template.cpp
@@ -1,30 +1,11 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <climits>
-#include <set>
-#include <algorithm>
-#include <unordered_map>
-#include <queue>
-#include <iomanip>
-#include <map>
-#include <utility>
-#include <numeric>
-#include <chrono>
-#include <ctime>
-#include <cassert>
-#include <bitset>
-#include <iterator>
-#include <cmath>
-#include <stack>
-#include <random>
+#include <bits/stdc++.h>
 
 #define rep(i,n) for(int i=0; i<(int)(n); i++)
 #define debug(x) cerr << #x << ": " << x << ", "
 #define debugln(x) cerr << #x << ": " << x << '\n'
 
-template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
-template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 
 using namespace std;
 using ll = long long;
