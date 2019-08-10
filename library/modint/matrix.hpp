f29603cb4811deb61376aceda95aa8490176d684
Akira Ajisaka <aajisaka@apache.org>
2019-08-10 18:39:53 +0900

    Make matrix library template

--- library/modint/matrix.hpp
+++ library/modint/matrix.hpp
@@ -1,29 +1,26 @@
 // Matrix library
-
-template<ll mod=mod17>
-struct matrix {
-    // return a * b where a and b are n * n matrix
-    vector<mint<mod>> mat_mul(const vector<mint<mod>>& a, const vector<mint<mod>>& b, int n) {
-      vector<mint<mod>> ret(n*n);
-      rep(i, n) rep(j, n) rep(k, n) ret[i*n+j] += a[i*n+k]*b[k*n+j];
-      return ret;
-    }
-
-    // return identity matrix of size n * n
-    vector<mint<mod>> id_mat(int n) {
-      vector<mint<mod>> ret(n*n);
-      rep(i, n) ret[i*n+i] = 1;
-      return ret;
-    }
-
-    // return a^x where a is n * n matrix
-    // a is changed, so do not use &a
-    vector<mint<mod>> mat_pow(vector<mint<mod>> a, unsigned long long x, int n) {
-      vector<mint<mod>> ret = id_mat(n);
-      while(x>0) {
-        if (x&1) ret = mat_mul(ret, a, n);
-        a = mat_mul(a, a, n); x>>=1;
+template<typename T>
+T mat_mul(const T& a, const T& b, int n) {
+  T ret(n*n);
+  for(int i=0; i<n; i++) {
+    for(int j=0; j<n; j++) {
+      for(int k=0; k<n; k++) {
+        ret[i*n+j] += a[i*n+k]*b[k*n+j];
       }
-      return ret;
     }
-};
+  }
+  return ret;
+}
+
+// return a^x where a is n * n matrix
+// a is changed, so do not use &a
+template<typename T>
+T mat_pow(T a, unsigned long long x, int n) {
+  T ret(n*n);
+  for(int i=0; i<n; i++) ret[i*n+i] = 1;
+  while(x>0) {
+    if (x&1) ret = mat_mul(ret, a, n);
+    a = mat_mul(a, a, n); x>>=1;
+  }
+  return ret;
+}
