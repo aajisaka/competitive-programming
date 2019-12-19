26695435e08875b032d26b42f1115c922b2cbc7d
Akira Ajisaka <aajisaka@apache.org>
2019-12-19 20:49:20 +0900

    Update dijkstra.cpp to better copy-and-pastability

--- library/graph/dijkstra.cpp
+++ library/graph/dijkstra.cpp
@@ -1,35 +1,19 @@
-#include <vector>
-#include <queue>
-using namespace std;
+vector<ll> dp(MAX_V);
+// P(to, cost)
+vector<P> graph[MAX_V];
 
-// Use first for the cost, second for the point to sort by the cost
-typedef pair<int, int> P;
-struct edge {
-    int to, cost;
-};
-int V=100;
-vector<edge> G[101]; // graph
-int d[101]; // d[i] is the distance between s to i
-
-// s is the starting point
-void dijkstra(int s) {
-  // Initialize d[i]. Initialization can be done outside this method.
-  fill(d, d+V, INT_MAX);
-  d[s] = 0;
-
-  priority_queue<P, vector<P>, greater<>> queue;
-  queue.push(P(0, s));
-  while (!queue.empty()) {
-    P p = queue.top(); queue.pop();
-    int u = p.second;
-    if (d[u] < p.first) {
-      continue;
-    }
-    for (edge e : G[u]) {
-      if (d[u] + e.cost < d[e.to]) {
-        d[e.to] = d[u] + e.cost;
-        queue.push(P(d[e.to], e.to));
-      }
+// copy-and-paste begin
+priority_queue<P, vector<P>, greater<>> que;
+que.emplace(0, 0);
+dp[0] = 0;
+while(!que.empty()) {
+  auto p = que.top(); que.pop();
+  if (p.first > dp[p.second]) continue;
+  for(const auto& e: graph[p.second]) {
+    if (dp[e.first] > p.first + e.second) {
+      dp[e.first] = p.first + e.second;
+      que.emplace(dp[e.first], e.first);
     }
   }
 }
+// copy-and-paste end
