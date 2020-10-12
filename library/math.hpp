766ed0475fc0fb324ebe12e5411eb644a4131c3b
Akira Ajisaka <aajisaka@apache.org>
2020-10-13 01:34:26 +0900

    Add sqrt_mod
    Verified: https://judge.yosupo.jp/submission/26553

--- library/math.hpp
+++ library/math.hpp
@@ -4,6 +4,9 @@
 #include <cassert>
 #include <tuple>
 #include <vector>
+#include <chrono>
+#include <random>
+#include <utility>
 
 long long pow_mod(long long x, long long n, int m) {
   assert(0 <= n && 1 <= m);
@@ -23,6 +26,60 @@ long long inv_mod(long long x, long long m) {
   auto z = internal::inv_gcd(x, m);
   assert(z.first == 1);
   return z.second;
+}
+
+long long norm_mod(long long a, long long p){
+  a%=p;
+  if(a<0)a+=p;
+  return a;
+}
+
+// p: odd (not necessarily prime)
+long long jacobi(long long a, long long p) {
+  a = norm_mod(a, p);
+  auto neg = [](long long x) { return x % 2 ? -1 : 1; };
+  if (a == 0) return p == 1;
+  else if (a % 2) return neg(((p - 1) & (a - 1)) >> 1) * jacobi(p % a, a);
+  else return neg(((p & 15) * (p & 15) - 1) / 8) * jacobi(a / 2, p);
+}
+
+long long rand_int(long long l, long long r) { //[l, r]
+#ifdef LOCAL
+  static std::mt19937_64 gen;
+#else
+  static std::mt19937_64 gen(std::chrono::steady_clock::now().time_since_epoch().count());
+#endif
+  return std::uniform_int_distribution<>(l, r)(gen);
+}
+
+// p: prime
+// 0 <= a < p
+// returns minimum solution
+// returns -1 if not exist
+long long sqrt_mod(long long a, long long p) {
+  assert(0 <= a);
+  assert(a < p);
+  if (p == 2)return a % 2;
+  if (a == 0)return 0;
+  if (jacobi(a, p) == -1)return -1;
+  long long b, c;
+  do {
+    b = rand_int(0, p - 1);
+    c = norm_mod(b * b - a, p);
+  } while (jacobi(c, p) != -1);
+  auto mul = [&](std::pair<long long, long long> x, std::pair<long long, long long> y) {
+      return std::pair<int, int>(norm_mod(x.first * y.first + x.second * y.second % p * c, p),
+                                 norm_mod(x.first * y.second + x.second * y.first, p));
+  };
+  std::pair<long long, long long> x(b, 1), cur(1, 0);
+  long long n = (p + 1) / 2;
+  while (n) {
+    if (n & 1)cur = mul(cur, x);
+    x = mul(x, x);
+    n >>= 1;
+  }
+  assert(cur.second == 0);
+  return std::min(cur.first, p - cur.first);
 }
 
 // (rem, mod)
